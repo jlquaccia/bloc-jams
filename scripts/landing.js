ff863d00b25549c8b66b1cfd0f9772d63caffd3b
Jason Quaccia <jlquaccia@gmail.com>
2016-03-23 21:40:45 -0700

    checkpoint 23 DOM scripting: events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
+var animatePoints = function(points) {
     function revealPoint(index) {
         var index = 0;
         
@@ -17,4 +17,17 @@ var animatePoints = function() {
     revealPoint(points);
 };
 
-animatePoints();
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+           animatePoints(pointsArray);
+       }
+    });
+}
